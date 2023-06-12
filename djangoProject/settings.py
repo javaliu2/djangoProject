35bb7ac8dbb51b7295b0fb7397919a3e956ac861
javaliu2 <55903897+javaliu2@users.noreply.github.com>
2023-06-12 10:34:56 +0800

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import progressBar.apps
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
